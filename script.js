cf1a37085cae111de5ed11e1e55ebbeae79df5bb
gustavoriveraa <gustavo.rivera.v@mail.pucv.cl>
2025-09-25 03:56:13 -0300

    actualizacion 3

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // -------------------
   // Fila 1
 const c2 = document.getElementById("c2");
 const c3 = document.getElementById("c3");
@@ -8,9 +7,8 @@ document.querySelector("#c1 .action-btn").addEventListener("click", () => {
   c3.classList.toggle("hidden");
 });
 
-  // -------------------
   // Fila 2
-  // -------------------
+
   const c4 = document.getElementById("c4");
   const c6 = document.getElementById("c6");
   document.querySelector("#c5 .action-btn").addEventListener("click", () => {
@@ -18,9 +16,9 @@ document.querySelector("#c1 .action-btn").addEventListener("click", () => {
     c6.classList.toggle("hidden");
   });
 
-  // -------------------
+
   // Fila 3
-  // -------------------
+
   const c7 = document.getElementById("c7");
   const c8 = document.getElementById("c8");
   document.querySelector("#c9 .action-btn").addEventListener("click", () => {
@@ -28,9 +26,9 @@ document.querySelector("#c1 .action-btn").addEventListener("click", () => {
     c8.classList.toggle("hidden");
   });
 
-  // -------------------
+ 
   // Fila 4
-  // -------------------
+
   const c11 = document.getElementById("c11");
   const c12 = document.getElementById("c12");
   document.querySelector("#c10 .action-btn").addEventListener("click", () => {
@@ -38,9 +36,9 @@ document.querySelector("#c1 .action-btn").addEventListener("click", () => {
     c12.classList.toggle("hidden");
   });
 
-  // -------------------
-  // Fila 5 (nueva)
-  // -------------------
+
+  // Fila 5 
+ 
   const c14 = document.getElementById("c14");
   const c15 = document.getElementById("c15");
   document.querySelector("#c13 .action-btn").addEventListener("click", () => {
@@ -48,9 +46,9 @@ document.querySelector("#c1 .action-btn").addEventListener("click", () => {
     c15.classList.toggle("hidden");
   });
 
-  // -------------------
+
   // Cuadro independiente
-  // -------------------
+
   const independent = document.querySelector("#ind1");
   if (independent.querySelector(".action-btn")) {
     independent.querySelector(".action-btn").addEventListener("click", () => {
